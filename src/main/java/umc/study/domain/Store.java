5a1bf365b927f483ea3a7906894d4bfef53f9e1d
양효빈 <hyobin3026@naver.com>
2024-11-21 11:17:54 +0900

    refactor: 로그 출력 로직 추가

--- src/main/java/umc/study/domain/Store.java
+++ src/main/java/umc/study/domain/Store.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import umc.study.domain.common.BaseEntity;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Builder
@@ -26,4 +29,21 @@ public class Store extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id")
     private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", score=" + score +
+                ", region=" + (region != null ? region.getName() : "N/A") + // region의 이름 출력
+                '}';
+    }
 }
